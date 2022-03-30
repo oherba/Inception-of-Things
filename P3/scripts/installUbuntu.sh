78664c5af5c4ac98c0b348493f36705e5c84101a
Othman Herba <oherba@e1r13p3.1337.ma>
2022-03-30 19:28:05 +0000

    clean up P3

--- P3/scripts/installUbuntu.sh
+++ P3/scripts/installUbuntu.sh
@@ -18,10 +18,11 @@ VBoxManage modifyvm $MACHINENAME --ioapic on
 VBoxManage modifyvm $MACHINENAME --memory 4096 --vram 128 --cpus 4
 VBoxManage modifyvm $MACHINENAME --nic1 nat
 VBoxManage modifyvm $MACHINENAME --natpf1 "guestssh,tcp,,2222,,22"
+VBoxManage modifyvm $MACHINENAME --natpf1 "http,tcp,,80,,80"
 #Create Disk and connect Debian Iso
-VBoxManage createhd --filename "$BASEFOLDER"/$MACHINENAME/$MACHINENAME_DISK.vdi --size 120000 --format VDI
+VBoxManage createhd --filename "$BASEFOLDER"/$MACHINENAME_DISK.vdi --size 120000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$BASEFOLDER"/$MACHINENAME/$MACHINENAME_DISK.vdi
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$BASEFOLDER"/$MACHINENAME_DISK.vdi
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $WORKDIR/Ubuntu_64.iso
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
--- P3/scripts/setup.sh
+++ P3/scripts/setup.sh
@@ -1,7 +1,5 @@
-k3d cluster create  -p "80:80@loadbalancer"  -p "8888:8888@loadbalancer"  -p "30002:30002@loadbalancer"  --wait
+k3d cluster create  -p "80:80@loadbalancer"  -p "8888:8888@loadbalancer"   --wait
 kubectl apply -f confs/namespc.yaml
-kubectl apply -f confs/gitlab-dep.yaml
-kubectl wait --for=condition=Ready pods --all -n gitlab
 kubectl apply -f confs/install-argocd.yaml -n argocd
 kubectl wait --for=condition=Ready pods --all -n argocd
 kubectl apply -f confs/ingress.yaml -n argocd
